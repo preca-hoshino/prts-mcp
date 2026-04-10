7e86beb0975e76fb20973ed39ddcbc87a8729ad8
preca-hoshino <001@labmem.com.cn>
2026-04-10 21:26:06 +0800

    [Add](core): Add request handler

---
+++ src/handler.rs
@@ -0,0 +1,45 @@
+//! Request handlers for the MCP Server.
+
+use crate::tools::HelloWorldTools;
+use async_trait::async_trait;
+use rust_mcp_sdk::{
+    McpServer,
+    mcp_server::ServerHandler,
+    schema::{
+        CallToolRequestParams, CallToolResult, ListToolsResult, PaginatedRequestParams, RpcError,
+        schema_utils::CallToolError,
+    },
+};
+use std::sync::Arc;
+
+/// A custom handler implementing the `ServerHandler` trait for this server.
+#[derive(Debug)]
+pub struct MyServerHandler;
+
+#[async_trait]
+impl ServerHandler for MyServerHandler {
+    async fn handle_list_tools_request(
+        &self,
+        _params: Option<PaginatedRequestParams>,
+        _runtime: Arc<dyn McpServer>,
+    ) -> std::result::Result<ListToolsResult, RpcError> {
+        Ok(ListToolsResult {
+            meta: None,
+            next_cursor: None,
+            tools: HelloWorldTools::tools(),
+        })
+    }
+
+    async fn handle_call_tool_request(
+        &self,
+        params: CallToolRequestParams,
+        _runtime: Arc<dyn McpServer>,
+    ) -> std::result::Result<CallToolResult, CallToolError> {
+        let tool_params: HelloWorldTools =
+            HelloWorldTools::try_from(params).map_err(CallToolError::new)?;
+
+        match tool_params {
+            HelloWorldTools::HelloWorldTool(tool) => tool.call_tool(),
+        }
+    }
+}
